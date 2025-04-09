dbbf1da0fb590d11ca9b3d5c706372819b6acf68
dongho <dho0647@naver.com>
2025-04-09 16:07:38 +0900

    feat: DeadLetterQueue를 이용한 재처리

---
+++ src/main/java/org/seeroo/hellomessagequeue/config/RabbitMQManualConfig.java
@@ -0,0 +1,21 @@
+package org.seeroo.hellomessagequeue.config;
+
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableRabbit
+@Configuration
+public class RabbitMQManualConfig {
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        // 수정 모드 설정
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        return factory;
+    }
+}
---
+++ src/main/java/org/seeroo/hellomessagequeue/consumer/OrderConsumer.java
@@ -0,0 +1,46 @@
+package org.seeroo.hellomessagequeue.consumer;
+
+import com.rabbitmq.client.Channel;
+import org.seeroo.hellomessagequeue.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OrderConsumer {
+    private static final int MAX_RETRIES = 3; // 총 시도 제한 수
+    private int retryCount = 0; // 재시도 횟수
+
+    @RabbitListener(queues = RabbitMQConfig.ORDER_COMPLETED_QUEUE, containerFactory = "rabbitListenerContainerFactory")
+    public void processOrder(String message, Channel channel, @Header("amqp_deliveryTag") long tag) {
+        try {
+            // 실패 유발
+            if ("fail".equalsIgnoreCase(message)) {
+                if (retryCount < MAX_RETRIES) {
+                    System.out.println("#### Fail & Retry = " + retryCount);
+                    retryCount++;
+                    throw new RuntimeException(message);
+                } else {
+                    System.out.println("#### 최대 횟수 초과, DLQ 이동 시킴");
+                    retryCount = 0;
+                    channel.basicNack(tag, false, false);
+                    return;
+                }
+            }
+
+            System.out.println("#성공 : " + message);
+            channel.basicAck(tag, false);
+            retryCount = 0;
+        } catch (Exception e) {
+            System.out.println("# error 발생 : " + e.getMessage());
+            try {
+                // 실패 시 basicReject 재처리 전송
+                channel.basicReject(tag, true);
+            } catch (IOException e1) {
+                System.out.println("# fail # reject message : " + e1.getMessage());
+            }
+        }
+    }
+}
